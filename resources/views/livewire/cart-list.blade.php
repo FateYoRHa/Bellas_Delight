bfc0a321f09a271d576fd943011c080819dff35c
FateYoRHa <20182921@s.ubaguio.edu>
2021-12-02 23:11:43 +0800

    terms and contidtions

--- resources/views/livewire/cart-list.blade.php
+++ resources/views/livewire/cart-list.blade.php
@@ -9,49 +9,57 @@
     </h3>
     <div class="flex-1">
         <table class="w-full text-sm lg:text-base" cellspacing="0">
-            <thead>
-                <tr class="h-12 uppercase">
-                    <th class="md:table-cell"></th>
-                    <th class="text-left">Product Name</th>
-                    <th class="pl-5 text-left lg:text-right lg:pl-0">Quantity
-                    </th>
-                    <th class="text-right md:table-cell">Price/Total</th>
-                    <th class="hidden text-right md:table-cell">Remove </th>
-                </tr>
-            </thead>
-            <tbody>
-                @foreach ($cartItems as $item)
-                    <tr>
-                        <td>
-                            <img src="{{ $item['attributes']['image'] }}" class="w-20 rounded" alt="Thumbnail">
-                        </td>
-                        <td>
-                            {{ $item['name'] }}
-                        </td>
-                        <td>
-                            <livewire:cart-update :item="$item" :key="$item['id']" />
-                        </td>
-                        <td class="text-right md:table-cell">
-                            <span class="text-sm font-medium lg:text-base">
-                                ₱{{ $item['price'] * $item['quantity'] }}
-                            </span>
-                        </td>
-                        <td class="text-right md:table-cell">
-                            <button class="px-4 py-2 text-white bg-red-600"
-                                wire:click.prevent="removeCart('{{ $item['id'] }}')">x</button>
-                        </td>
+            @if (Cart::getCount() > 0)
+                <thead>
+                    <tr class="h-12 uppercase">
+                        <th class="md:table-cell"></th>
+                        <th class="text-left">Product Name</th>
+                        <th class="pl-5 text-left lg:text-right lg:pl-0">Quantity
+                        </th>
+                        <th class="text-right md:table-cell">Price/Total</th>
+                        <th class="hidden text-right md:table-cell">Remove </th>
                     </tr>
-                @endforeach
+                </thead>
+                <tbody>
 
-            </tbody>
+                    @foreach ($cartItems as $item)
+                        <tr>
+                            <td>
+                                <img src="{{ $item['attributes']['image'] }}" class="w-20 rounded" alt="Thumbnail">
+                            </td>
+                            <td>
+                                {{ $item['name'] }}
+                            </td>
+                            <td>
+                                <livewire:cart-update :item="$item" :key="$item['id']" />
+                            </td>
+                            <td class="text-right md:table-cell">
+                                <span class="text-sm font-medium lg:text-base">
+                                    ₱{{ $item['price'] * $item['quantity'] }}
+                                </span>
+                            </td>
+                            <td class="text-right md:table-cell">
+                                <button class="px-4 py-2 text-white bg-red-600"
+                                    wire:click.prevent="removeCart('{{ $item['id'] }}')">x</button>
+                            </td>
+                        </tr>
+
+                    @endforeach
+                </tbody>
         </table>
         <div>
             Total: ₱{{ Cart::getTotal() }}
         </div>
         <div class="mt-5">
             <button class="px-6 py-2 text-red-800 bg-red-300" wire:click.prevent="clearAllCart">Remove All Cart</button>
-            <a href="checkout" class="btn float-end"><button class="px-6 py-2 text-black-800 bg-green-300">Check Out</button></a>
+            <a href="checkout" class="btn float-end"><button class="px-6 py-2 text-black-800 bg-green-300">Check
+                    Out</button></a>
         </div>
 
+
+    @else
+        <a href="{{ route('product-menu') }}" class="btn btn-success px-6 py-2 text-black-800"><button>Back to
+                Shopping</button></a>
+        @endif
     </div>
 </div>
