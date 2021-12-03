44e08eea1a8aacf9edc5eb687da8755f9996ceaf
FateYoRHa <20182921@s.ubaguio.edu>
2021-12-03 14:09:37 +0800

    j

--- resources/views/livewire/cart-list.blade.php
+++ resources/views/livewire/cart-list.blade.php
@@ -4,12 +4,13 @@
             <p class="text-green-800">{{ $message }}</p>
         </div>
     @endif
+    @if (Cart::getCount() > 0)
     <h3 class="text-3xl text-bold">
         Total of {{ Cart::getCount() }} Item/s in Cart
     </h3>
     <div class="flex-1">
         <table class="w-full text-sm lg:text-base" cellspacing="0">
-            @if (Cart::getCount() > 0)
+
                 <thead>
                     <tr class="h-12 uppercase">
                         <th class="md:table-cell"></th>
@@ -39,8 +40,11 @@
                                 </span>
                             </td>
                             <td class="text-right md:table-cell">
-                                <button class="px-4 py-2 text-white bg-red-600"
-                                    wire:click.prevent="removeCart('{{ $item['id'] }}')">x</button>
+                                <button class="px-2 py-2 bg-red-500"
+                                    wire:click.prevent="removeCart('{{ $item['id'] }}')"><svg width="15" height="15" fill="currentColor" class="bi bi-trash" viewBox="0 0 15 15">
+                                        <path d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0V6z"/>
+                                        <path fill-rule="evenodd" d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1v1zM4.118 4 4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4H4.118zM2.5 3V2h11v1h-11z"/>
+                                      </svg></button>
                             </td>
                         </tr>
 
@@ -58,6 +62,9 @@
 
 
     @else
+    <h3 class="text-3xl text-bold text-center">
+        Total of {{ Cart::getCount() }} Item/s in Cart
+    </h3>
         <a href="{{ route('product-menu') }}" class="btn btn-success px-6 py-2 text-black-800"><button>Back to
                 Shopping</button></a>
         @endif
